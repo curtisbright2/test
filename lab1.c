de223f5967110c69835d0bb1a7756578c744d0d6
curtisbright2 <113138287+curtisbright2@users.noreply.github.com>
2022-09-08 15:21:38 -0400

    Create lab1.c

---
+++ lab1.c
@@ -0,0 +1,21 @@
+/* ===========================================================================
+COMP-1410 Lab 1
+=========================================================================== */
+
+#include <stdio.h>
+#include <assert.h>
+
+// digit_sum_iterative(n) returns the decimal sum of the digits in n
+// requires: 0 <= n < 10^9
+// note: implemented using iteration and not recursion
+int digit_sum_iterative(int n);
+
+// digit_sum_recursive(n) returns the decimal sum of the digits in n
+// requires: 0 <= n < 10^9
+// note: implemented using recursion and not iteration
+int digit_sum_recursive(int n);
+
+int main(void) {
+    // Implement your test code here
+    printf("All tests passed successfully.\n");
+}
